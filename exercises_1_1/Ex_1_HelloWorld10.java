737c3167e055074d3865deae8bd9db34affae99f
Tristan Burgess <tburgessdev@gmail.com>
2020-11-21 14:26:48 -0500

    Section 1.1 coding exercises from the book

---
+++ exercises_1_1/Ex_1_HelloWorld10.java
@@ -0,0 +1,9 @@
+package exercises_1_1;
+
+public class Ex_1_HelloWorld10 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello, world!");
+        }
+    }
+}
